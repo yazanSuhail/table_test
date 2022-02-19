bd735922fcc86f71fcde9dbc5dadfacdfa978f72
yazanSuhail <76630403+yazanSuhail@users.noreply.github.com>
2022-02-19 12:24:17 -0300

    Update TaskAssignment.js

--- src/components/TaskAssignment/TaskAssignment.js
+++ src/components/TaskAssignment/TaskAssignment.js
@@ -1,58 +1,91 @@
-import { useState } from "react"
-export function TaskAssignment ({users, tasks, setTasks, setShowElement}){
-const [ userInput, setUserInput ] = useState({
-    title:'',
-    user:'',
-    complate: false
-})
+import { useState } from "react";
 
-const handleChange = (obj) =>{
-    setUserInput(obj)
-} 
+const initialTask = {
+  title: "",
+  user: "",
+  complate: false,
+  userId: ""
+};
 
-const handleSubmit = () =>{
-    if(userInput.user !== '' && userInput.user !== "Selecione um usuário" && userInput.title !== '')
-    {
-        setTasks([...tasks, userInput])
+export function TaskAssignment({ users, tasks, setTasks, setShowElement }) {
+  const [task, setTask] = useState(initialTask);
 
+  const handleChange = (obj) => {
+    setTask(obj);
+  };
+
+  const handleChangeName = (name) => {
+    const id = users.find((user) => user.name === name).id;
+
+    setTask((prev) => {
+      return {
+        ...prev,
+        user: name,
+        userId: id
+      };
+    });
+  };
+
+  const handleSubmit = () => {
+    if (
+      task.user !== "" &&
+      task.user !== "Selecione um usuário" &&
+      task.title !== ""
+    ) {
+      setTasks([...tasks, task]);
     }
-}
-const handleClose = ()=>{
-    setShowElement(false) 
-}
- return(
-     <>
-    
-     <p>Task Assignment</p>
-     <form onSubmit={(e) => {e.preventDefault(); handleSubmit()}}>
-     <label htmlFor="user">User: </label>
-     <select name="user" id="user" defaultValue={userInput.user}
-         onChange={ (e)=> {
-            handleChange({...userInput, user: e.target.value })
-          }} > <option value="Selecionar Usuário"
-          >Selecionar Usuário</option>
-{users.map((user)=> {
-    return   <option  key={user.name} value={user.name}>{user.name}</option>
-})}
-    </select>
+  };
 
-  Title  <input value={userInput.title} 
-  onChange={ (e)=> {
-    handleChange({...userInput, title: e.target.value })
-  }}
-  />
-  Completa <input type="checkbox" value={userInput.completed}
-    onChange={ (e)=> {
-        console.log(e.target.checked)
-        handleChange({...userInput, completed: e.target.checked })
-      }}/>
-  <input type="submit" value="adicionar"/>
-</form>
-<button 
-onClick={handleClose}
->Close</button>
+  const handleClose = () => {
+    setShowElement(false);
+  };
 
-     </>
- )   
-
+  return (
+    <>
+      <p>Task Assignment</p>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <label htmlFor="user">User: </label>
+        <select
+          name="user"
+          id="user"
+          defaultValue={task.user}
+          onChange={(e) => {
+            handleChangeName(e.target.value);
+          }}
+        >
+          {" "}
+          <option value="Selecionar Usuário">Selecionar Usuário</option>
+          {users.map(({ name, id }) => {
+            return (
+              <option key={id} value={name}>
+                {name}
+              </option>
+            );
+          })}
+        </select>
+        Title{" "}
+        <input
+          value={task.title}
+          onChange={(e) => {
+            handleChange({ ...task, title: e.target.value });
+          }}
+        />
+        Completa{" "}
+        <input
+          type="checkbox"
+          value={task.completed}
+          onChange={(e) => {
+            handleChange({ ...task, completed: e.target.checked });
+          }}
+        />
+        <input type="submit" value="adicionar" />
+      </form>
+      <button onClick={handleClose}>Close</button>
+    </>
+  );
 }
