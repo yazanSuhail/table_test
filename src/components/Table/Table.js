c40556276fe64774d4a8d1028baa1ae581338fc6
yazanSuhail <76630403+yazanSuhail@users.noreply.github.com>
2022-02-19 12:23:44 -0300

    Update Table.js

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,47 +1,45 @@
 /* eslint-disable jsx-a11y/alt-text */
-import './table.css'
+import "./table.css";
+const trashcan = "./images/trash.svg";
 
-const Table = ({tasks, users, filterValue, setTasks}) => {
-  const deleteItem = (arr)=> {
-    setTasks(arr)
-  }
-return(
-  
-<>
-<table style={{width: '600px'}}>
-  <thead>
-  <tr>
-    <th>Usario</th>
-    <th>Titulo</th>
-    <th>Completa</th>
-    <th>Excluir</th>
-  </tr>
-  </thead>
-  <tbody>
-  {tasks.filter((task)=>{
-    return(
-      task.title.includes(filterValue) 
-    )
-  }).map( (task, index)=> {
-    return(
-      <tr key={`${task.title + index}`} >
-        <td>User {task.userId}</td>
-        <td>{task.title}</td>
-        <td>{ task.completed ? "Sim" : "Não"}</td>
-        <td><img src='./images/trash.svg' 
-        onClick={() => {
-          deleteItem(tasks.filter((value, filterIndex)=>{
-            return(index !== filterIndex)
-          }))
-        }}
-        /></td>
-      </tr>
-    )
-})}
-</tbody>
-</table>
+const Table = ({ tasks, filterValue, setTasks }) => {
+  const handleDelete = (id) => {
+    const newTasks = tasks.filter((value) => value.id !== id);
 
-</>);    
+    setTasks(newTasks);
+  };
 
-} 
+  return (
+    <>
+      <table style={{ width: "600px" }}>
+        <thead>
+          <tr>
+            <th>Usario</th>
+            <th>Titulo</th>
+            <th>Completa</th>
+            <th>Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks
+            .filter((task) => {
+              return task.title.includes(filterValue);
+            })
+            .map((task, i) => {
+              return (
+                <tr key={`${task.id}-${i}`}>
+                  <td>User {task.userId}</td>
+                  <td>{task.title}</td>
+                  <td>{task.completed ? "Sim" : "Não"}</td>
+                  <td>
+                    <img src={trashcan} onClick={() => handleDelete(task.id)} />
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </>
+  );
+};
 export default Table;
